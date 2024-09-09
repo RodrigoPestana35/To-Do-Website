7c2d88641afbef7569423c11640a78b694ed652e
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-09 02:10:30 +0100

    Refactor form input field ID and add task styling

--- Js.js
+++ Js.js
@@ -1,13 +1,14 @@
 document.getElementById('task_form').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    var task = document.getElementById('task').value;
+    var task = document.getElementById('inputTask').value;
     const taskList = document.getElementById('task_list');
     
     if(task == "") return;
 
     var li = document.createElement('li');
     li.textContent = task;
+    li.className = 'task';
     
     var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
@@ -19,5 +20,5 @@ document.getElementById('task_form').addEventListener('submit', function(e) {
     li.appendChild(deleteButton);
     taskList.appendChild(li);
 
-    document.getElementById('task').value = "";
+    document.getElementById('inputTask').value = "";
 });
